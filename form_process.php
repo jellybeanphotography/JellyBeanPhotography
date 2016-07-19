d1216e9ddb8854044eaf1f2d90a07a7cb296d18e
jellybeanphotography <jellybeanphotographer@gmail.com>
2016-07-19 14:33:49 +0100

    Uploaded current web files.

---
+++ form_process.php
@@ -0,0 +1,12 @@
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$tel = $_POST['tel'];
+$enquiry = $_POST['enquiry'];
+
+$to = "info@jellybeanphotography.me.uk";
+$subject = "JellyBean Photography Website Enquiry";
+
+mail ($to, $subject, $enquiry, "From: " . $name);
+header("Location: /thankyou.html"); /* Redirect Browser */
+exit();
