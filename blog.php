d82e5ed0469b066cb8626452d146e49843a1a6b7
jellybeanphotography <jellybeanphotographer@gmail.com>
2016-07-20 20:36:09 +0100

    Update Header & Footers
    Updated all the headers and footers to the new PHP Scripts.

--- blog.php
+++ blog.php
@@ -1,49 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <title>JellyBean Photography - Gloucestershire Freelance Photographer</title>
-    <link rel="stylesheet" type="text/css" href="normalise.css" media="screen">
-    <link href="https://fonts.googleapis.com/css?family=Exo+2:700" rel="stylesheet" type="text/css">
-    <link rel="stylesheet" type="text/css" href="style.css" media="screen">
-    <link rel="apple-touch-icon" sizes="57x57" href="images\favicon/apple-icon-57x57.png">
-    <link rel="apple-touch-icon" sizes="60x60" href="images\favicon/apple-icon-60x60.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="images\favicon/apple-icon-72x72.png">
-    <link rel="apple-touch-icon" sizes="76x76" href="images\favicon/apple-icon-76x76.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="images\favicon/apple-icon-114x114.png">
-    <link rel="apple-touch-icon" sizes="120x120" href="images\favicon/apple-icon-120x120.png">
-    <link rel="apple-touch-icon" sizes="144x144" href="images\favicon/apple-icon-144x144.png">
-    <link rel="apple-touch-icon" sizes="152x152" href="images\favicon/apple-icon-152x152.png">
-    <link rel="apple-touch-icon" sizes="180x180" href="images\favicon/apple-icon-180x180.png">
-    <link rel="icon" type="image/png" sizes="192x192"  href="images\favicon/android-icon-192x192.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="images\favicon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="96x96" href="images\favicon/favicon-96x96.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="images\favicon/favicon-16x16.png">
-    <link rel="manifest" href="images\favicon/manifest.json">
-    <meta name="msapplication-TileColor" content="#ffffff">
-    <meta name="msapplication-TileImage" content="images\favicon/ms-icon-144x144.png">
-    <meta name="theme-color" content="#ffffff">
-  </head>
-  <body>
-    <header>
-      <img src="images/Logo.svg" class="header-logo" alt="JellyBean Photography Logo" tite="JellyBean Photography Logo">
-      <div id="header-container-nav">
-        <ul id="nav">
-          <li><a href="index.html">Home</a></li>
-          <li><a href="services.html">Services</a></li>
-          <li><a href="about.html">About</a></li>
-          <li><a href="blog.html">Blog</a></li>
-          <li><a href="contact.html">Contact</a></li>
-        </ul>
-      </div>
-    </header>
+<?php include "partials/head.php"; ?>
+<?php include "partials/analytics.php"; ?>
 
-      <div class="socialiconscontainer">
-        <a href="https://www.facebook.com/JellyBeanPhotographer"><img src="images/icons/facebookicon.svg" class="socialicons" alt="Facebook Icon" title="Facebook Icon"></a>
-        <a href="https://twitter.com/scotttaylor85"><img src="images/icons/twittericon.svg" class="socialicons" alt="Twitter Icon" title="Twitter Icon"></a>
-        <a href="https://plus.google.com/u/0/+JellyBeanPhotography"><img src="images/icons/googleplusicon.svg" class="socialicons" alt="GooglePlus Icon" title="GooglePlus Icon"></a>
-        <a href="https://uk.linkedin.com/in/jellybeanphotographer"><img src="images/icons/linkedinicon.svg" class="socialicons" alt="LinkedIn Icon" title="LinkedIn Icon"></a>
-      </div>
+<body>
+  <?php include "partials/header.php"; ?>
+  <?php include "partials/socials.php"; ?>
 
       <section>
         <div id="pageheader">
@@ -76,8 +36,6 @@
 
       </section>
 
-  <footer>
-    <small>Scott Taylor | JellyBean Photography | 47 Somergate Road, Cheltenham, Glos, GL51 0QE | info@jellybeanphotography.me.uk</small>
-  </footer>
+  <?php include "partials/footer.php"; ?>
 </body>
 </html>
