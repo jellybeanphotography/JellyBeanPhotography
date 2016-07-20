47e6132e8af29a8937cd01cbd5fe75806c0a79b1
jellybeanphotography <jellybeanphotographer@gmail.com>
2016-07-20 14:48:20 +0100

    links in Nav changed to PHP
    links in Nav changed to PHP

--- partials/header.php
+++ partials/header.php
@@ -1,10 +1,10 @@
 <img src="images/Logo.svg" class="header-logo" alt="JellyBean Photography Logo" tite="JellyBean Photography Logo">
 <div id="header-container-nav">
   <ul id="nav">
-    <li><a href="index.html">Home</a></li>
-    <li><a href="services.html">Services</a></li>
-    <li><a href="about.html">About</a></li>
-    <li><a href="blog.html">Blog</a></li>
-    <li><a href="contact.html">Contact</a></li>
+    <li><a href="index.php">Home</a></li>
+    <li><a href="services.php">Services</a></li>
+    <li><a href="about.php">About</a></li>
+    <li><a href="blog.php">Blog</a></li>
+    <li><a href="contact.php">Contact</a></li>
   </ul>
 </div>
