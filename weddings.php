4fcd1211a35ddf56e711ca2974a87c8f120b919f
jellybeanphotography <jellybeanphotographer@gmail.com>
2016-07-26 00:49:51 +0100

    Completed wedding services Page
    Completed wedding services Page
    uploaded images for the weddings section and created a ‘contactusicon’
    for the services pages.

--- weddings.php
+++ weddings.php
@@ -9,8 +9,55 @@
         <div id="pageheader">
           <h2>Wedding Services...</h2>
         </div>
+        <div id="imageblock">
+          <img src="public/images/wedding/weddingimage001.jpg" alt="" title="">
+          <img src="public/images/wedding/weddingimage002.jpg" alt="" title="">
+          <img src="public/images/wedding/weddingimage003.jpg" alt="" title="">
+        </div>
         <div id="textblock">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          JellyBean Photography offers a modern and relaxed style of photography ensuring that your special day is captured to the highest possibly standard to provide memories for years to come. JellyBean Photography provide a friendly, proffesional photographer to make sure that you receive the best service to the whole process as easy as possible.
+          <p>We offer 2 options for your wedding day, we do this to ensure we cna offer services for all possible budgets.</p>
+          <div id="weddingpackagescontainer">
+            <h3>Option 1</h3>
+            <h4>Complete Package</h4>
+            <h3>£800 (All day Coverage)</h3>
+            <p>This package includes:</p>
+            <ul>
+              <li>Bridal Preperation</li>
+              <li>Groom Preperation</li>
+              <li>Ceremony</li>
+              <li>Reception Party</li>
+              <li>Group Shots</li>
+              <li>Couple Shots</li>
+              <li>Room Detail Shots</li>
+              <li>Speeches</li>
+              <li>Cake Cutting</li>
+              <li>First Dance</li>
+              <li>Evening Photography</li>
+            </ul>
+          </div>
+          <div id="weddingpackagescontainer">
+            <h3>Option 2</h3>
+            <h4>Tailor your own Package</h4>
+            <h3>£??? (Pick the areas you want)</h3>
+            <p>Areas and their prices:</p>
+            <ul>
+              <li>Bridal Preperation - £50</li>
+              <li>Groom Preperation - £50</li>
+              <li>Ceremony - £400</li>
+              <li>Reception Party - Included in Ceremony</li>
+              <li>Group Shots - £100</li>
+              <li>Couple Shots - £100</li>
+              <li>Speeches - £80</li>
+              <li>Cake Cutting - Included in Ceremony</li>
+              <li>First Dance - £80</li>
+              <li>Evening Photography - £100</li>
+              <li> - </li>
+            </ul>
+          </div>
+          <p>Throughout the whole process JellyBean Photography will offer meetings to ensure and reasure you of the whole photography process of your special day is planned to last detail and also to make plans for unforseeable occurances, weather it be the hottest day of the year the wettest day imaginable.</p>
+          <p>For more information on our wedding packages please contact using the link below:</p>
+          <?php include "partials/contactus.php"; ?>
         </div>
 
       </section>
