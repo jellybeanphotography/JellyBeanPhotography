ea05dbd37ae04f7a17e7a53239337a8f0ba709ff
jellybeanphotography <jellybeanphotographer@gmail.com>
2016-07-25 13:35:44 +0100

    Changes to Services Page
    Changes to Services Page

--- services.php
+++ services.php
@@ -10,7 +10,7 @@
       <h2>Services</h2>
     </div>
     <div id="textblock">
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      JellyBean Photography offers a variety of photographic services, if you require a services but isnt listed below please contact us and we can discuss your requirements to find a solution.
     </div>
     <?php include "partials/servicesiconscontainer.php"; ?>
 
