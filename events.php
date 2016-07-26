4b8668552facf567ff3da28f2ddb5dc570e44bcd
jellybeanphotography <jellybeanphotographer@gmail.com>
2016-07-26 21:47:42 +0100

    Creation of Portraits and Events pages
    Creation of Portraits and Events pages with all the media to complete
    the pages.

--- events.php
+++ events.php
@@ -5,14 +5,44 @@
   <?php include "partials/header.php"; ?>
   <?php include "partials/socials.php"; ?>
 
-  <section>
-    <div id="pageheader">
-      <h2>Events Services...</h2>
-    </div>
-    <div id="textblock">
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-    </div>
-  </section>
+      <section>
+        <div id="pageheader">
+          <h2>Events Services...</h2>
+        </div>
+        <div id="imageblock">
+          <img src="public/images/events/eventsimage001.jpg" alt="Wedding Party Band Shoot" title="Wedding Party Band Shoot">
+          <img src="public/images/events/eventsimage002.jpg" alt="Music Event Shoot" title="Music Event Shoot">
+          <img src="public/images/events/eventsimage003.jpg" alt="Charity Event Shoot" title="Charity Event Shoot">
+        </div>
+        <div id="textblock">
+          JellyBean Photography can cover all types of events from celebrations to birthday's to corporate and charity events too.
+          <h3>Package and Pricing</h3>
+          <p>We offer a simple single package for our events service.</p>
+          <div id="weddingpackagescontainer">
+            <h3>Events Package</h3>
+            <h4>Price includes covering the event and images</h4>
+            <h3>£150 (Per Event)</h3>
+            <p>This package includes:</p>
+            <ul>
+              <li>Coverage of the event</li>
+              <li>All edited images</li>
+            </ul>
+          </div>
+        </div>
+          <div id="textblock">
+            <h3>What will I receive...</h3>
+            <p>So once the event has finished we will get all the images edited and sorted then we will be intouch to arrange a meeting to present the final images to you.</p>
+            <ul>
+              <li>All edited images on USB Memory Stick.</li>
+              <li>A unique online gallery personalised to you for you and friends/family can view and download the full resolution images at not extra cost (included in package).</li>
+              <li>Full permission to Print and Copy any of the images.</li>
+            </ul>
 
-  <?php include "partials/footer.php"; ?>
+            <h4>For more information oabout the events service please contact using the link below:</h4>
+            <?php include "partials/contactus.php"; ?>
+        </div>
+
+      </section>
+
+      <?php include "partials/footer.php"; ?>
 </body>
--- portraits.php
+++ portraits.php
@@ -5,14 +5,45 @@
   <?php include "partials/header.php"; ?>
   <?php include "partials/socials.php"; ?>
 
-  <section>
-    <div id="pageheader">
-      <h2>Portraits Services...</h2>
-    </div>
-    <div id="textblock">
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-    </div>
-  </section>
+      <section>
+        <div id="pageheader">
+          <h2>Portraits Services...</h2>
+        </div>
+        <div id="imageblock">
+          <img src="public/images/portraits/portraitsimage001.jpg" alt="Studio Portrait Shoot" title="Studio Portrait Shoot">
+          <img src="public/images/portraits/portraitsimage002.jpg" alt="Bridal Studio Shoot" title="Bridal Studio Shoot">
+          <img src="public/images/portraits/portraitsimage003.jpg" alt="Child Portrait Shoot" title="Child Portrait Shoot">
+        </div>
+        <div id="textblock">
+          JellyBean Photography can capture fun and exciting times in the studio either indiviually or groups. I normally shoot on a white background but can accomodate black or grey backgrounds too. We can also shoot off location either at the clients home or we can shoot outdoors if client prefers.
+          <h3>Package and Pricing</h3>
+          <p>We offer a single package for our portraiture service.</p>
+          <div id="weddingpackagescontainer">
+            <h3>Portrait Package</h3>
+            <h4>Price includes sitting and images</h4>
+            <h3>£80 (Per Session)</h3>
+            <p>This package includes:</p>
+            <ul>
+              <li>The sitting</li>
+              <li>All edited images</li>
+              <li>Each session normally takes about an hour</li>
+            </ul>
+          </div>
+        </div>
+          <div id="textblock">
+            <h3>What will I receive...</h3>
+            <p>So once the shoot has finished we will get all the images edited and we will be intouch to arrange a meeting to present the final images to you.</p>
+            <ul>
+              <li>All edited images on USB Memory Stick.</li>
+              <li>A unique online gallery personalised to you for you and friends/family can view and download the full resolution images at not extra cost (included in package).</li>
+              <li>Full permission to Print and Copy any of the images.</li>
+            </ul>
 
-  <?php include "partials/footer.php"; ?>
+            <h4>For more information oabout the portrait service please contact using the link below:</h4>
+            <?php include "partials/contactus.php"; ?>
+        </div>
+
+      </section>
+
+      <?php include "partials/footer.php"; ?>
 </body>
