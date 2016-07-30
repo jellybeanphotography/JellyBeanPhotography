8f826c1c9d10083f54c99e3687a1409ea7663f85
jellybeanphotography <jellybeanphotographer@gmail.com>
2016-07-30 22:29:11 +0100

    Fixed Resize issue on Portfolio page
    Fixed Resize issue on Portfolio page

--- portfolio.php
+++ portfolio.php
@@ -12,9 +12,9 @@
           <div id="textblock">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
           </div>
-            <div id="portfoliocontainer">
-              <div id="imageholder"><img src="public\images/portfolio/blankimage.jpg" alt="" title=""></div>
-              <div id="imageholder"><img src="public\images/portfolio/blankimage.jpg" alt="" title=""></div>
+            <div id="imageholder">
+              <img src="public\images/portfolio/blankimage.jpg" alt="" title="">
+              <img src="public\images/portfolio/blankimage.jpg" alt="" title="">
             </div>
 
       </section>
