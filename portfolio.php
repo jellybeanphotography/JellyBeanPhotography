fbdf887fc93109cab80538ab94135fc77b60ce3d
jellybeanphotography <jellybeanphotographer@gmail.com>
2016-07-30 22:59:56 +0100

    Images added for Portfolio page
    Images added for Portfolio page

--- portfolio.php
+++ portfolio.php
@@ -13,9 +13,44 @@
               Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
           </div>
             <div id="imageholder">
-              <img src="public\images/portfolio/blankimage.jpg" alt="" title="">
-              <img src="public\images/portfolio/blankimage.jpg" alt="" title="">
+              <img src="public\images/portfolio/wedding/Wedding001.jpg" alt="" title="">
+              <img src="public\images/portfolio/wedding/Wedding002.jpg" alt="" title="">
+              <img src="public\images/portfolio/wedding/Wedding003.jpg" alt="" title="">
+              <img src="public\images/portfolio/wedding/Wedding004.jpg" alt="" title="">
+              <img src="public\images/portfolio/wedding/Wedding005.jpg" alt="" title="">
+              <img src="public\images/portfolio/wedding/Wedding006.jpg" alt="" title="">
+              <img src="public\images/portfolio/wedding/Wedding007.jpg" alt="" title="">
+              <img src="public\images/portfolio/wedding/Wedding008.jpg" alt="" title="">
             </div>
+              <div id="imageholder">
+                <img src="public\images/portfolio/events/events001.jpg" alt="" title="">
+                <img src="public\images/portfolio/events/events002.jpg" alt="" title="">
+                <img src="public\images/portfolio/events/events003.jpg" alt="" title="">
+                <img src="public\images/portfolio/events/events004.jpg" alt="" title="">
+                <img src="public\images/portfolio/events/events005.jpg" alt="" title="">
+                <img src="public\images/portfolio/events/events006.jpg" alt="" title="">
+                <img src="public\images/portfolio/events/events007.jpg" alt="" title="">
+                <img src="public\images/portfolio/events/events008.jpg" alt="" title="">
+                <img src="public\images/portfolio/events/events009.jpg" alt="" title="">
+                <img src="public\images/portfolio/events/events010.jpg" alt="" title="">
+                <img src="public\images/portfolio/events/events011.jpg" alt="" title="">
+                <img src="public\images/portfolio/events/events012.jpg" alt="" title="">
+                <img src="public\images/portfolio/events/events013.jpg" alt="" title="">
+                <img src="public\images/portfolio/events/events014.jpg" alt="" title="">
+                <img src="public\images/portfolio/events/events015.jpg" alt="" title="">
+                <img src="public\images/portfolio/events/events016.jpg" alt="" title="">
+                <img src="public\images/portfolio/events/events017.jpg" alt="" title="">
+                <img src="public\images/portfolio/events/events018.jpg" alt="" title="">
+                <img src="public\images/portfolio/events/events019.jpg" alt="" title="">
+              </div>
+                <div id="imageholder">
+                  <img src="public\images/portfolio/portraits/portraits001.jpg" alt="" title="">
+                  <img src="public\images/portfolio/portraits/portraits002.jpg" alt="" title="">
+                  <img src="public\images/portfolio/portraits/portraits003.jpg" alt="" title="">
+                  <img src="public\images/portfolio/portraits/portraits004.jpg" alt="" title="">
+                  <img src="public\images/portfolio/portraits/portraits005.jpg" alt="" title="">
+                  <img src="public\images/portfolio/portraits/portraits006.jpg" alt="" title="">
+                </div>
 
       </section>
 
