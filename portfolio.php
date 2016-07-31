fbb3133003de2b4f448d8c2084896bf0b9813621
jellybeanphotography <jellybeanphotographer@gmail.com>
2016-07-31 22:39:46 +0100

    Description added to Portfolio page and CSS modified to match
    Description added to Portfolio page and CSS modified to match.

--- portfolio.php
+++ portfolio.php
@@ -10,46 +10,48 @@
           <h2>Portfolio</h2>
         </div>
           <div id="textblock">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              Here are a selections of images I have taken from Weddings to Events to portraits and more, no two shoots are ever the same and all these special moments are recorded for you to look back and re-experience the moments all over again.
+              <p>If you have liked what you have seen plese check out my <a href="about.php">About Page</a>.
+                 If you have any questions about any of the areas of photography please contact us via the <a href="contact.php">Contact Page</a> and we will get back you to help get your special moment captured.</p>
           </div>
             <div id="imageholder">
-              <img src="public\images/portfolio/wedding/Wedding001.jpg" alt="" title="">
-              <img src="public\images/portfolio/wedding/Wedding002.jpg" alt="" title="">
-              <img src="public\images/portfolio/wedding/Wedding003.jpg" alt="" title="">
-              <img src="public\images/portfolio/wedding/Wedding004.jpg" alt="" title="">
-              <img src="public\images/portfolio/wedding/Wedding005.jpg" alt="" title="">
-              <img src="public\images/portfolio/wedding/Wedding006.jpg" alt="" title="">
-              <img src="public\images/portfolio/wedding/Wedding007.jpg" alt="" title="">
-              <img src="public\images/portfolio/wedding/Wedding008.jpg" alt="" title="">
+              <img src="public\images/portfolio/wedding/Wedding001.jpg" alt="JellyBean Photography Portfolio Image" title="JellyBean Photography Portfolio Image">
+              <img src="public\images/portfolio/wedding/Wedding002.jpg" alt="JellyBean Photography Portfolio Image" title="JellyBean Photography Portfolio Image">
+              <img src="public\images/portfolio/wedding/Wedding003.jpg" alt="JellyBean Photography Portfolio Image" title="JellyBean Photography Portfolio Image">
+              <img src="public\images/portfolio/wedding/Wedding004.jpg" alt="JellyBean Photography Portfolio Image" title="JellyBean Photography Portfolio Image">
+              <img src="public\images/portfolio/wedding/Wedding005.jpg" alt="JellyBean Photography Portfolio Image" title="JellyBean Photography Portfolio Image">
+              <img src="public\images/portfolio/wedding/Wedding006.jpg" alt="JellyBean Photography Portfolio Image" title="JellyBean Photography Portfolio Image">
+              <img src="public\images/portfolio/wedding/Wedding007.jpg" alt="JellyBean Photography Portfolio Image" title="JellyBean Photography Portfolio Image">
+              <img src="public\images/portfolio/wedding/Wedding008.jpg" alt="JellyBean Photography Portfolio Image" title="JellyBean Photography Portfolio Image">
             </div>
               <div id="imageholder">
-                <img src="public\images/portfolio/events/events001.jpg" alt="" title="">
-                <img src="public\images/portfolio/events/events002.jpg" alt="" title="">
-                <img src="public\images/portfolio/events/events003.jpg" alt="" title="">
-                <img src="public\images/portfolio/events/events004.jpg" alt="" title="">
-                <img src="public\images/portfolio/events/events005.jpg" alt="" title="">
-                <img src="public\images/portfolio/events/events006.jpg" alt="" title="">
-                <img src="public\images/portfolio/events/events007.jpg" alt="" title="">
-                <img src="public\images/portfolio/events/events008.jpg" alt="" title="">
-                <img src="public\images/portfolio/events/events009.jpg" alt="" title="">
-                <img src="public\images/portfolio/events/events010.jpg" alt="" title="">
-                <img src="public\images/portfolio/events/events011.jpg" alt="" title="">
-                <img src="public\images/portfolio/events/events012.jpg" alt="" title="">
-                <img src="public\images/portfolio/events/events013.jpg" alt="" title="">
-                <img src="public\images/portfolio/events/events014.jpg" alt="" title="">
-                <img src="public\images/portfolio/events/events015.jpg" alt="" title="">
-                <img src="public\images/portfolio/events/events016.jpg" alt="" title="">
-                <img src="public\images/portfolio/events/events017.jpg" alt="" title="">
-                <img src="public\images/portfolio/events/events018.jpg" alt="" title="">
-                <img src="public\images/portfolio/events/events019.jpg" alt="" title="">
+                <img src="public\images/portfolio/events/events001.jpg" alt="JellyBean Photography Portfolio Image" title="JellyBean Photography Portfolio Image">
+                <img src="public\images/portfolio/events/events002.jpg" alt="JellyBean Photography Portfolio Image" title="JellyBean Photography Portfolio Image">
+                <img src="public\images/portfolio/events/events003.jpg" alt="JellyBean Photography Portfolio Image" title="JellyBean Photography Portfolio Image">
+                <img src="public\images/portfolio/events/events004.jpg" alt="JellyBean Photography Portfolio Image" title="JellyBean Photography Portfolio Image">
+                <img src="public\images/portfolio/events/events005.jpg" alt="JellyBean Photography Portfolio Image" title="JellyBean Photography Portfolio Image">
+                <img src="public\images/portfolio/events/events006.jpg" alt="JellyBean Photography Portfolio Image" title="JellyBean Photography Portfolio Image">
+                <img src="public\images/portfolio/events/events007.jpg" alt="JellyBean Photography Portfolio Image" title="JellyBean Photography Portfolio Image">
+                <img src="public\images/portfolio/events/events008.jpg" alt="JellyBean Photography Portfolio Image" title="JellyBean Photography Portfolio Image">
+                <img src="public\images/portfolio/events/events009.jpg" alt="JellyBean Photography Portfolio Image" title="JellyBean Photography Portfolio Image">
+                <img src="public\images/portfolio/events/events010.jpg" alt="JellyBean Photography Portfolio Image" title="JellyBean Photography Portfolio Image">
+                <img src="public\images/portfolio/events/events011.jpg" alt="JellyBean Photography Portfolio Image" title="JellyBean Photography Portfolio Image">
+                <img src="public\images/portfolio/events/events012.jpg" alt="JellyBean Photography Portfolio Image" title="JellyBean Photography Portfolio Image">
+                <img src="public\images/portfolio/events/events013.jpg" alt="JellyBean Photography Portfolio Image" title="JellyBean Photography Portfolio Image">
+                <img src="public\images/portfolio/events/events014.jpg" alt="JellyBean Photography Portfolio Image" title="JellyBean Photography Portfolio Image">
+                <img src="public\images/portfolio/events/events015.jpg" alt="JellyBean Photography Portfolio Image" title="JellyBean Photography Portfolio Image">
+                <img src="public\images/portfolio/events/events016.jpg" alt="JellyBean Photography Portfolio Image" title="JellyBean Photography Portfolio Image">
+                <img src="public\images/portfolio/events/events017.jpg" alt="JellyBean Photography Portfolio Image" title="JellyBean Photography Portfolio Image">
+                <img src="public\images/portfolio/events/events018.jpg" alt="JellyBean Photography Portfolio Image" title="JellyBean Photography Portfolio Image">
+                <img src="public\images/portfolio/events/events019.jpg" alt="JellyBean Photography Portfolio Image" title="JellyBean Photography Portfolio Image">
               </div>
                 <div id="imageholder">
-                  <img src="public\images/portfolio/portraits/portraits001.jpg" alt="" title="">
-                  <img src="public\images/portfolio/portraits/portraits002.jpg" alt="" title="">
-                  <img src="public\images/portfolio/portraits/portraits003.jpg" alt="" title="">
-                  <img src="public\images/portfolio/portraits/portraits004.jpg" alt="" title="">
-                  <img src="public\images/portfolio/portraits/portraits005.jpg" alt="" title="">
-                  <img src="public\images/portfolio/portraits/portraits006.jpg" alt="" title="">
+                  <img src="public\images/portfolio/portraits/portraits001.jpg" alt="JellyBean Photography Portfolio Image" title="JellyBean Photography Portfolio Image">
+                  <img src="public\images/portfolio/portraits/portraits002.jpg" alt="JellyBean Photography Portfolio Image" title="JellyBean Photography Portfolio Image">
+                  <img src="public\images/portfolio/portraits/portraits003.jpg" alt="JellyBean Photography Portfolio Image" title="JellyBean Photography Portfolio Image">
+                  <img src="public\images/portfolio/portraits/portraits004.jpg" alt="JellyBean Photography Portfolio Image" title="JellyBean Photography Portfolio Image">
+                  <img src="public\images/portfolio/portraits/portraits005.jpg" alt="JellyBean Photography Portfolio Image" title="JellyBean Photography Portfolio Image">
+                  <img src="public\images/portfolio/portraits/portraits006.jpg" alt="JellyBean Photography Portfolio Image" title="JellyBean Photography Portfolio Image">
                 </div>
 
       </section>
