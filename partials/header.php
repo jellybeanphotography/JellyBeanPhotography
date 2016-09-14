52b75cbf51ef726f08868d38f3dda91f201faca0
jellybeanphotography <jellybeanphotographer@gmail.com>
2016-09-14 22:22:22 +0100

    addition of new Menu

--- partials/header.php
+++ partials/header.php
@@ -1,6 +1,7 @@
 <img src="public\images/Logo.svg" class="header-logo" alt="JellyBean Photography Logo" tite="JellyBean Photography Logo">
-<div id="header-container-nav">
-  <ul id="nav">
+<img src="public\images/threelines.png" id="open_menu">
+<nav>
+  <ul>
     <li><a href="index.php">Home</a></li>
     <li><a href="services.php">Services</a></li>
     <li><a href="portfolio.php">Portfolio</a></li>
@@ -8,4 +9,9 @@
     <li><a href="blog.php">Blog</a></li>
     <li><a href="contact.php">Contact</a></li>
   </ul>
-</div>
+</nav>
+<script>
+  $(function() {
+    $("#open_menu").click(function() { $("nav").toggleClass("menuactive");});
+  });
+</script>
