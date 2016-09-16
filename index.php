a964917f6e16fae71242f4c7c0e44f964060ba17
jellybeanphotography <jellybeanphotographer@gmail.com>
2016-09-16 15:42:23 +0100

    Minor Index page adjustments and Content
    Content added to the Index page and 2 image divs created on the index
    page.

--- index.php
+++ index.php
@@ -7,7 +7,16 @@
   <section>
     <h1>Gloucestershire Freelance Photographer</h1>
     <h2>For all your Photography Needs.</h2>
-    <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</h3>
+    <h3>Looking for a photographer? Then look no further… JellyBean Photography can provide photography services for all areas of photography. A Gloucestershire Freelance Photographer offering photography services throughout Gloucestershire area.
+        <p>Whether it’s your wedding day, anniversary or a special birthday celebration JellyBean Photography can help and all with tailored packages to make sure you get exactly what you’re looking for and at a price you can be happy with.
+        <p>JellyBean Photography can also offer photography for corporate events, websites, events, sports events and even portraits for your business website to give it a fresh update.
+        <p>How about something new! We can offer a unique portrait session whether it be for your modelling portfolio or just for giggle and laugh with your friends and mates, JellyBean Photography can offer it all.
+        <p>Or maybe you want to be a family portrait photo for a keepsake for memories for years to come, we can offer multiple packages to help get what you are looking for.
+    </h3>
+    <div class="imageseperator1"></div>
+    <h4>A festival covered by JellyBean Photography</h4>
+    <div class="imageseperator2"></div>
+    <h4>A speedway media event covered by JellyBean Photography</h4>
   </section>
 
   <?php include "partials/footer.php"; ?>
