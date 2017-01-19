39eb2ccc1291104175fb724e7bfea4b82ef711fe
jellybeanphotography <jellybeanphotographer@gmail.com>
2017-01-19 20:36:33 +0000

    Link modification for home in navigation
    Link modification for home in navigation

--- partials/header.php
+++ partials/header.php
@@ -4,7 +4,7 @@
   <nav>
     <img src="public/images/menu-logo.svg" class="menu-logo" alt="JellyBean Photography Logo" title="JellyBean Photography Logo">
     <ul>
-      <li><a href="index.php">Home</a></li>
+      <li><a href="/">Home</a></li>
       <li><a href="services.php">Services</a></li>
       <li><a href="portfolio.php">Portfolio</a></li>
       <li><a href="about.php">About</a></li>
