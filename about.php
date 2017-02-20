408e80d994d291054ff6f1b2e912cd270cfcf90d
jellybeanphotography <jellybeanphotographer@gmail.com>
2017-02-20 21:45:56 +0000

    Fullscreen image for About Page Added
    Fullscreen image for About Page Added

--- about.php
+++ about.php
@@ -2,6 +2,7 @@
 
 <body>
   <?php include "partials/header.php"; ?>
+  <style>.fullscreenimage{background-image: url(/public/images/fullscreenimage/about.jpg);}</style>
 
       <section>
         <h1>About us...</h1>
