27c058b20f4a7993ec79a51042e47699189a7d2c
jellybeanphotography <jellybeanphotographer@gmail.com>
2017-04-05 00:50:25 +0100

    Facebook link in Header.php change
    Facebook link in Header.php change

--- partials/header.php
+++ partials/header.php
@@ -12,7 +12,7 @@
       <li><a href="contact">CONTACT</a></li>
     </ul>
     <div class="socialiconscontainer">
-      <a href="https://www.facebook.com/JellyBeanPhotographer" target="_blank"><img src="public/images/icons/facebookicon.svg" class="socialicons" alt="Facebook Icon" title="Facebook Icon"></a>
+      <a href="https://www.facebook.com/JellyBeanPhotography.me.uk" target="_blank"><img src="public/images/icons/facebookicon.svg" class="socialicons" alt="Facebook Icon" title="Facebook Icon"></a>
       <a href="https://twitter.com/scottatJBP" target="_blank"><img src="public/images/icons/twittericon.svg" class="socialicons" alt="Twitter Icon" title="Twitter Icon"></a>
       <a href="https://plus.google.com/u/0/+JellyBeanPhotography" target="_blank"><img src="public/images/icons/googleplusicon.svg" class="socialicons" alt="GooglePlus Icon" title="GooglePlus Icon"></a>
       <a href="https://uk.linkedin.com/in/jellybeanphotographer" target="_blank"><img src="public/images/icons/linkedinicon.svg" class="socialicons" alt="LinkedIn Icon" title="LinkedIn Icon"></a>
